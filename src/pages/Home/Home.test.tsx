13c626a9cad532cfdbc41cc298dc8e4d1d75527e
Wendell Christian <atendimento.wendellc@gmail.com>
2025-10-21 12:20:46 -0300

    fix: update name

--- src/pages/Home/Home.test.tsx
+++ src/pages/Home/Home.test.tsx
@@ -2,38 +2,43 @@ import { render, screen } from '@testing-library/react';
 import { Home } from './';
 
 jest.mock('@/components/UserSearch', () => ({
-    UserSearch: () => (
-        <form className="form-inline d-flex justify-content-between flex-column w-100">
-            <div className="w-100 d-flex justify-content-between">
-                <input
-                    type="text"
-                    className="form-control mb-2 me-2 py-3"
-                    placeholder="Usuário do Github"
-                />
-                <button type="submit" className="btn d-flex align-items-center btn-primary mb-2 h-auto">
-                    Pesquisar
-                </button>
-            </div>
-        </form>
-    ),
+  UserSearch: () => (
+    <form className="form-inline d-flex justify-content-between flex-column w-100">
+      <div className="w-100 d-flex justify-content-between">
+        <input
+          type="text"
+          className="form-control mb-2 me-2 py-3"
+          placeholder="Usuário do Github"
+        />
+        <button
+          type="submit"
+          className="btn d-flex align-items-center btn-primary mb-2 h-auto"
+        >
+          Pesquisar
+        </button>
+      </div>
+    </form>
+  ),
 }));
 
 describe('Pages :: Home', () => {
-    it('should render the title and subtitle', () => {
-        render(<Home />);
+  it('should render the title and subtitle', () => {
+    render(<Home />);
 
-        const titleElement = screen.getByText(/Github Software/i);
-        expect(titleElement).toBeInTheDocument();
+    const titleElement = screen.getByText(/Github Search/i);
+    expect(titleElement).toBeInTheDocument();
 
-        const subtitleElement = screen.getByText(/Buscador de Usuários no Github/i);
-        expect(subtitleElement).toBeInTheDocument();
-    });
+    const subtitleElement = screen.getByText(/Buscador de Usuários no Github/i);
+    expect(subtitleElement).toBeInTheDocument();
+  });
 
-    it('should render the UserSearch component', () => {
-        render(<Home />);
+  it('should render the UserSearch component', () => {
+    render(<Home />);
 
-        const userSearchElement = screen.getByPlaceholderText(/Usuário do Github/i);
-        expect(userSearchElement).toBeInTheDocument();
-        expect(screen.getByRole('button', { name: /Pesquisar/i })).toBeInTheDocument();
-    });
+    const userSearchElement = screen.getByPlaceholderText(/Usuário do Github/i);
+    expect(userSearchElement).toBeInTheDocument();
+    expect(
+      screen.getByRole('button', { name: /Pesquisar/i })
+    ).toBeInTheDocument();
+  });
 });
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,11 @@
 import { UserSearch } from '@/components/UserSearch';
 
 export const Home = () => {
-    return (
-        <div className="d-flex flex-column justify-content-center align-items-center">
-            <h1 className="pt-5 fw-bold fs-1 fs-md-2">
-                Github Software
-            </h1>
-            <h3 className="py-3 fs-5 fs-md-4">
-                Buscador de Usuários no Github
-            </h3>
-            <UserSearch />
-        </div>
-
-    );
+  return (
+    <div className="d-flex flex-column justify-content-center align-items-center">
+      <h1 className="pt-5 fw-bold fs-1 fs-md-2">Github Search</h1>
+      <h3 className="py-3 fs-5 fs-md-4">Buscador de Usuários no Github</h3>
+      <UserSearch />
+    </div>
+  );
 };
